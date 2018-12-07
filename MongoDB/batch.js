ce56e1c538d41906f437809b53c54b7da623ceb5
Ayoub Oudmane <ayoub@oudmane.me>
2018-12-07 15:29:03 +0300

    #

--- MongoDB/batch.js
+++ MongoDB/batch.js
@@ -2,79 +2,53 @@ const {
   IDRegex, ObjectID
 } = MongoDB = require('./index'),
   sift = require('sift').default,
-  batches = new Map(),
-  resolvedPromise = Promise.resolve(),
-  dispatch = batch => {
-    process.nextTick(
-      () => {
-        const queue = batch.queue
-        batch.queue = []
-        const keys = queue.map(({ key }) => key)
-        MongoDB.getConnection(batch.database).collection(batch.collection).find(
-          {
-            _id: {
-              $in: keys
-            }
-          }
-        ).toArray().then(
-          documents => {
-            queue.forEach(
-              ({ key, resolve }) =>
-                resolve(
-                  sift(
-                    {
-                      _id: key
-                    },
-                    documents
-                  ).shift()
-                )
-            )
-          }
-        ).catch(
-          error =>
-            queue.forEach(
-              ({ reject }) =>
-                reject(
-                  error
-                )
-            )
-        )
-      }
-    )
-  }
-getBatch = (collection, database) => {
-  const key = [collection, database].join(':')
-  return batches.get(key) || batches.set(
-    key,
-    {
-      collection,
-      database,
-      cache: new Map(),
-      queue: [],
-      load(key) {
-        let promise = this.cache.get(key)
-        if (!promise) {
-          promise = new Promise(
-            (resolve, reject) => {
-              this.queue.push({ key, resolve, reject })
-            }
-          ).then(
-            () =>
-              this.cache.delete(key)
-          )
-          this.cache.set(key, promise)
-          if (this.queue.length === 1)
-            dispatch(this)
-        }
-        return promise
-      }
-    }
-  ).get(key)
-}
+  Dataloader = require('dataloader'),
+  batches = new Map()
 
 module.exports = class MongoDBBatch {
   static load(id, collection, database = 'default') {
-    return getBatch(collection, database).load(IDRegex.test(id) ? ObjectID(id) : id)
+    let key = ['load', database, collection].join(':'),
+      batch = batches.get(key)
+    if (!batch) {
+      batches.set(
+        key,
+        batch = new Dataloader(
+          keys =>
+            MongoDB.getConnection(database).collection(collection).find(
+              {
+                _id: {
+                  $in: keys.map(
+                    id =>
+                      IDRegex.test(id) ? ObjectID(id) : id
+                  )
+                }
+              }
+            ).toArray().then(
+              documents =>
+                keys.map(
+                  key =>
+                    sift(
+                      {
+                        _id: key
+                      },
+                      documents
+                    ).shift()
+                )
+            )
+        )
+      )
+    }
+    return batch.load(id).then(
+      document => {
+        batch.clear(id)
+        return document
+      }
+    ).catch(
+      error => {
+        batch.clear(id)
+        return Promise.reject(error)
+      }
+    )
   }
   static loadMany(ids, collection, database = 'default') {
     return Promise.all(
@@ -83,5 +57,87 @@ module.exports = class MongoDBBatch {
           this.load(id, collection, database)
       )
     )
+  }
+  static insert(object, collection, database = 'default') {
+    let key = ['insert', database, collection].join(':'),
+      batch = batches.get(key)
+    if (!batch) {
+      batches.set(
+        key,
+        batch = new Dataloader(
+          documents => {
+            const bulk = MongoDB.getConnection(database).collection(collection).initializeOrderedBulkOp()
+            documents.forEach(
+              object =>
+                bulk.insert(object)
+            )
+            return bulk.execute().then(
+              response =>
+                response.getInsertedIds().map(id => id._id.toHexString())
+            )
+          },
+          {
+            cache: false
+          }
+        )
+      )
+    }
+    return batch.load(object)
+  }
+  static update(id, payload, collection, database = 'default') {
+    let key = ['update', database, collection].join(':'),
+      batch = batches.get(key)
+    if (!batch) {
+      batches.set(
+        key,
+        batch = new Dataloader(
+          payloads => {
+            const bulk = MongoDB.getConnection(database).collection(collection).initializeOrderedBulkOp()
+            payloads.forEach(
+              ({ id, payload }) =>
+                bulk.find({
+                  _id: IDRegex.test(id) ? ObjectID(id) : id
+                }).updateOne(payload)
+            )
+            return bulk.execute().then(
+              response =>
+                payloads.map(a => true)
+            )
+          },
+          {
+            cache: false
+          }
+        )
+      )
+    }
+    return batch.load({ id, payload })
+  }
+  static upsert(id, payload, collection, database = 'default') {
+    let key = ['upsert', database, collection].join(':'),
+      batch = batches.get(key)
+    if (!batch) {
+      batches.set(
+        key,
+        batch = new Dataloader(
+          payloads => {
+            const bulk = MongoDB.getConnection(database).collection(collection).initializeOrderedBulkOp()
+            payloads.forEach(
+              ({ id, payload }) =>
+                bulk.find({
+                  _id: IDRegex.test(id) ? ObjectID(id) : id
+                }).upsert().replaceOne(payload)
+            )
+            return bulk.execute().then(
+              response =>
+                payloads.map(a => true)
+            )
+          },
+          {
+            cache: false
+          }
+        )
+      )
+    }
+    return batch.load({ id, payload })
   }
 }
